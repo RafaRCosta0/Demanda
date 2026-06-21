f3c5ff1f5fd76e0fca445f3c07019cb44991428c
Rafael <rafaelribeirodacosta24@gmail.com>
2026-06-21 16:05:34 -0300

    Alteração de paleta e detalhes

--- Js/script.js
+++ Js/script.js
@@ -6,27 +6,35 @@ let btnProximo = document.getElementById("btn-proximo");
 let btnAnterior = document.getElementById("btn-anterior");
 
 let imagens = [
-"../assets/banner1.png",
-"../assets/banner3.png",
-"../assets/banner2 (2).png"
+    "../assets/img60.jpeg",
+    "../assets/img65.jpeg",
+    "../assets/img70.jpeg"
 ];
 
 let index = 0;
 
-btnProximo.addEventListener("click", function() {
-    index = index + 1;
+function proximoSlide() {
+    index++;
 
     if (index >= imagens.length) {
         index = 0;
     }
-    imagem.src = imagens[index];
-});
 
-btnAnterior.addEventListener("click", function() {
-    index = index - 1;
+    imagem.src = imagens[index];
+}
+
+function anteriorSlide() {
+    index--;
 
     if (index < 0) {
         index = imagens.length - 1;
     }
+
     imagem.src = imagens[index];
-});
+}
+
+btnProximo.addEventListener("click", proximoSlide);
+btnAnterior.addEventListener("click", anteriorSlide);
+
+// Trocar automaticamente a cada 5 segundos de slide
+setInterval(proximoSlide, 5000);
