4e416e967eac2fb11e1bb01389a922731ca64a8d
breno almeida silva lima <brenoalmida2020@gmail.com>
2026-06-21 22:57:49 -0300

    Atualização paleta de cores parte Breno

--- Js/search.js
+++ Js/search.js
@@ -1,47 +1,15 @@
-function toggleLista(id) {
-    const lista = document.getElementById(id);
-    
-    if (!lista) {
-        console.error("Produto não encontrado!!: " + id);
-        return;
-    }
-
-    if (lista.style.display === "none" || lista.style.display === "") {
-        lista.style.display = "block";
-        lista.style.transition = "all 0.5s ease";
-    } else {
-        lista.style.display = "none";
-    }
-}
-
-function mudar(botao, delta) {
-    const display = botao.parentElement.querySelector('.val');
-    if (display) {
-        let valor = parseInt(display.innerText);
-        valor += delta;
-        if (valor < 0) valor = 0;
-        display.innerText = valor;
-    }
-}
-fetch("./dados.json")
-
+fetch("../html/dados.json")
 .then(response => response.json())
-.then(data => {
+.then(produtos => {
 
-    const produtos = document.querySelectorAll(".produto-linha")
-    produtos.forEach(item => {
-        const titulo = item.querySelector("h3")
-        const nome = titulo.textContent.trim()
-        data.forEach(produto => {
-            if(nome === produto.nome){
-                const img = document.createElement("img")
-                img.src = produto.imagem
-                img.classList.add("img-produto")
-                titulo.parentElement.prepend(img)
-            }   
+    const imagens = document.querySelectorAll(".produto-img");
 
-        })
-
-    })
+    produtos.forEach((produto, index) => {
+        if(imagens[index]){
+            imagens[index].src = produto.imagem;
+            imagens[index].alt = "Imagem do produto";
+        }
+    });
 
 })
+.catch(error => console.log(error));
